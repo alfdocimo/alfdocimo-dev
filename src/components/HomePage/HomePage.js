2916d68f9fb152dd83e4d4772e78cbd6f2b131be
Alfredo Narvaez <alfredo.narvaez@travelport.com>
2019-10-28 18:03:27 +0100

    feat: start implementing selectors from state

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+
 import { Layout, Breadcrumb } from "antd";
 import SideMenu from "../SideMenu";
 
 const { Header, Content, Footer, Sider } = Layout;
 
-
 const HomePage = () => {
+  // eslint-disable-next-line no-console
+  const test = useSelector(state => state.homePageReducer.vm.test);
+
   const [state, setState] = useState({
     collapsed: false
   });
@@ -30,9 +34,7 @@ const HomePage = () => {
             Bill is a cat.
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
+        <Footer style={{ textAlign: "center" }}>{test}</Footer>
       </Layout>
     </Layout>
   );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
-import { combineReducers } from 'redux';
-import fuelSavings from './fuelSavingsReducer';
-import { connectRouter } from 'connected-react-router'
+import { combineReducers } from "redux";
+// import fuelSavings from './fuelSavingsReducer';
+import { connectRouter } from "connected-react-router";
+import homePageReducer from "./homePageReducer";
 
-const rootReducer = history => combineReducers({
-  router: connectRouter(history),
-  fuelSavings,
-});
+const rootReducer = history =>
+  combineReducers({
+    router: connectRouter(history),
+    // fuelSavings, // example code
+    homePageReducer
+  });
 
 export default rootReducer;
