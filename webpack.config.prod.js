3283ebc244f8b0c089819368179f583d5639f12b
Alfredo Narvaez <alfredo.narvaez@travelport.com>
2019-11-29 20:33:59 +0100

    fix: less loader

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,31 +1,32 @@
 // For info about this file refer to webpack and webpack-hot-middleware documentation
 // For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import path from 'path';
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
+import theme from "./src/styles/vars";
 
 const GLOBALS = {
-  'process.env.NODE_ENV': JSON.stringify('production'),
+  "process.env.NODE_ENV": JSON.stringify("production"),
   __DEV__: false
 };
 
 export default {
   resolve: {
-    extensions: ['*', '.js', '.jsx', '.json'],
+    extensions: ["*", ".js", ".jsx", ".json"],
     // To support react-hot-loader
     alias: {
-      'react-dom': '@hot-loader/react-dom'
+      "react-dom": "@hot-loader/react-dom"
     }
   },
-  devtool: 'source-map', // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
-  entry: path.resolve(__dirname, 'src/index'),
-  target: 'web',
-  mode: 'production',
+  devtool: "source-map", // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
+  entry: path.resolve(__dirname, "src/index"),
+  target: "web",
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].[contenthash].js'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].[contenthash].js"
   },
   plugins: [
     // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
@@ -33,13 +34,13 @@ export default {
 
     // Generate an external css file with a hash in the filename
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
+      filename: "[name].[contenthash].css"
     }),
 
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
-      template: 'src/index.ejs',
-      favicon: 'src/favicon.ico',
+      template: "src/index.ejs",
+      favicon: "src/favicon.ico",
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -55,24 +56,23 @@ export default {
       inject: true,
       // Note that you can add custom options here if you need to handle other custom logic in index.html
       // To track JavaScript errors via TrackJS, sign up for a free trial at TrackJS.com and enter your token below.
-      trackJSToken: ''
-    }),
-
+      trackJSToken: ""
+    })
   ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ["babel-loader"]
       },
       {
         test: /\.eot(\?v=\d+.\d+.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              name: '[name].[ext]'
+              name: "[name].[ext]"
             }
           }
         ]
@@ -81,11 +81,11 @@ export default {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000,
-              mimetype: 'application/font-woff',
-              name: '[name].[ext]'
+              mimetype: "application/font-woff",
+              name: "[name].[ext]"
             }
           }
         ]
@@ -94,11 +94,11 @@ export default {
         test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000,
-              mimetype: 'application/octet-stream',
-              name: '[name].[ext]'
+              mimetype: "application/octet-stream",
+              name: "[name].[ext]"
             }
           }
         ]
@@ -107,11 +107,11 @@ export default {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10000,
-              mimetype: 'image/svg+xml',
-              name: '[name].[ext]'
+              mimetype: "image/svg+xml",
+              name: "[name].[ext]"
             }
           }
         ]
@@ -120,35 +120,38 @@ export default {
         test: /\.(jpe?g|png|gif|ico)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]'
+              name: "[name].[ext]"
             }
           }
         ]
       },
       {
-        test: /(\.css|\.scss|\.sass)$/,
+        test: /(\.css|\.scss|\.sass|\.less)$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true
             }
-          }, {
-            loader: 'postcss-loader',
+          },
+          {
+            loader: "postcss-loader",
             options: {
-              plugins: () => [
-                require('cssnano'),
-                require('autoprefixer'),
-              ],
+              plugins: () => [require("autoprefixer")],
               sourceMap: true
             }
-          }, {
-            loader: 'sass-loader',
+          },
+          {
+            loader: "less-loader", // compiles Less to CSS
             options: {
-              includePaths: [path.resolve(__dirname, 'src')],
+              modifyVars: {
+                ...theme
+              },
+              javascriptEnabled: true,
+              includePaths: [path.resolve(__dirname, "src", "less")],
               sourceMap: true
             }
           }
