7a10fb5a46264c9c515f0f8f109148a144e2b558
Alfredo Narvaez <alfredo.narvaez@travelport.com>
2019-12-02 22:13:57 +0100

    feat: script for build

---
+++ tools/deployGithubPages.js
@@ -0,0 +1,15 @@
+import ghpages from "gh-pages";
+
+/**
+ * If `git` is not on your path, provide the path as shown below.
+ */
+ghpages.publish("dist", {
+  branch: "master",
+  repo: "https://github.com/alfdocimo/alfdocimo-dev",
+  message: "Auto-generated commit (deploy build)",
+  user: {
+    name: "alfdocimo",
+    email: "alfdocimo@gmail.com",
+    password: process.env.GITHUB_PASSWORD
+  }
+});
